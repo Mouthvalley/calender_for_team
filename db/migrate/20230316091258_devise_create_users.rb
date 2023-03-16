797b38353c03553a02248739c6a8a49d69ee5e8e
Mouthvalley <45648573+Mouthvalley@users.noreply.github.com>
2023-03-16 09:42:38 +0000

    [3/16]

---
+++ db/migrate/20230316091258_devise_create_users.rb
@@ -0,0 +1,28 @@
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false 
+      t.boolean :status, null: false,  default: false
+      t.timestamps null: false, default: ->{ "CURRENT_TIMESTAMP" }
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
